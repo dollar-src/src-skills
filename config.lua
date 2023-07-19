4141a3a2369485135e0b1323e93105ae1a61b952
FilipeCuco <filipecuco0@gmail.com>
2023-07-19 23:14:34 +0100

    better code

--- config.lua
+++ config.lua
@@ -5,6 +5,19 @@ Config.EnableCommand = true
 Config.Command = 'skill'
 Config.SkillTitle = 'Skills'
 
+Config.Levels = {
+   { min = 0, max = 100 }, -- level 1
+   { min = 100, max = 200 }, -- level 2
+   { min = 200, max = 400 }, -- level 3
+   { min = 400, max = 800 }, -- level 4
+   { min = 800, max = 1600 }, -- level 5
+   { min = 1600, max = 3200 }, -- level 6
+   { min = 3200, max = 6400 }, -- level 7
+   { min = 6400, max = 12800 }, -- level 8
+   { min = 12800, max = 25600 }, -- level 9
+   { min = 25600, max = 51200 } -- level 10
+}
+
 Config.Skills = {
   ['Mining'] = {
      ["Current"] = 0,
